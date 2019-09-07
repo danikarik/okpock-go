f56e869ef8bef5822f67b8839e2b49316d98f16d
danikarik <baitursynov92@gmail.com>
2019-09-08 03:07:45 +0600

    update migrations script

--- migrations/down.sql
+++ migrations/down.sql
@@ -5,3 +5,11 @@ DROP TABLE IF EXISTS `registrations`;
 DROP TABLE IF EXISTS `logs`;
 
 DROP TABLE IF EXISTS `users`;
+
+DROP TABLE IF EXISTS `user_projects`;
+
+DROP TABLE IF EXISTS `projects`;
+
+DROP TABLE IF EXISTS `user_uploads`;
+
+DROP TABLE IF EXISTS `uploads`;
